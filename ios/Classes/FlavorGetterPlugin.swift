2c0be3f0c76bd920f545bbf2075b900e1b41e0f7
Momar Cisse <momar@thepinkchannel.com>
2023-06-21 15:25:17 +0000

    Add metadata to swift and kotlin files

--- ios/Classes/FlavorGetterPlugin.swift
+++ ios/Classes/FlavorGetterPlugin.swift
@@ -1,3 +1,15 @@
+/*
+ * FlavorGetterPlugin.kt
+ * Flavor Getter Plugin
+ *
+ * Description: A plugin to retrieve the current flavor of the Flutter application.
+ * Version: 1.0.0
+ * Author: Momar Talla Cisse
+ * License: BSD 3-Clause
+ * Created: 2023-06-18
+ * Last Modified: 2023-06-21
+ */
+
 import Flutter
 import UIKit
 
